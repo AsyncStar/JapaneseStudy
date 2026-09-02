c74feacf5a633beaef50ac256f89e88039a4a29b
AsyncStar <sarai.s.johnson@gmail.com>
2026-09-02 00:41:42 -0700

    Condensed conjugator data schema

--- grammarData.js
+++ grammarData.js
@@ -1,81 +1,63 @@
-export const verbConjugations = {
-    standard: {
-        present: {
-            polite: {
-                affirmative: [
-                    {endingKana: "る", result: "Ichidan: ます | Godan: ります"},
-                    {endingKana: "む", result: "みます"}, {endingKana: "く", result: "きます"},
-                    {endingKana: "ぐ", result: "ぎます"}, {endingKana: "ぶ", result: "びます"},
-                    {endingKana: "ぬ", result: "にます"}, {endingKana: "う", result: "います"},
-                    {endingKana: "つ", result: "ちます"},
-                ],
-                negative: [
-                    {endingKana: "る", ichidanResult: "ません", godanResult: "りません"},
-                    {endingKana: "む", result: "みません"}, {endingKana: "く", result: "きません"},
-                    {endingKana: "ぐ", result: "ぎません"}, {endingKana: "ぶ", result: "びません"},
-                    {endingKana: "ぬ", result: "にません"}, {endingKana: "う", result: "いません"},
-                    {endingKana: "つ", result: "ちません"}],
-            },
+export const verbVowelChanges = {
+        dictionary: [
+            {endingKana: "る", result: "る"},
+            {endingKana: "む", result: "む"}, {endingKana: "く", result: "く"},
+            {endingKana: "ぐ", result: "ぐ"}, {endingKana: "ぶ", result: "ぶ"},
+            {endingKana: "ぬ", result: "ぬ"}, {endingKana: "う", result: "う"},
+            {endingKana: "つ", result: "つ"}],
+        iRow: [
+            {endingKana: "む", result: "み"}, {endingKana: "く", result: "き"},
+            {endingKana: "ぐ", result: "ぎ"}, {endingKana: "ぶ", result: "び"},
+            {endingKana: "ぬ", result: "に"}, {endingKana: "う", result: "い"},
+            {endingKana: "つ", result: "ち"},
 
-            short: {
-                affirmative: [
-                    {endingKana: "る", ichidanResult: "た　", godanResult: "った"},
-                    {endingKana: "む", result: "した"}, {endingKana: "く", result: "いた"},
-                    {endingKana: "ぐ", result: "いだ"}, {endingKana: "ぶ", result: "んだ"},
-                    {endingKana: "ぬ", result: "んだ"}, {endingKana: "う", result: "った"},
-                    {endingKana: "つ", result: "った"}],
+        ],
+        aRow: [
+            {endingKana: "る", result: "ら"},
+            {endingKana: "む", result: "ま"}, {endingKana: "く", result: "か"},
+            {endingKana: "ぐ", result: "が"}, {endingKana: "ぶ", result: "ば"},
+            {endingKana: "ぬ", result: "な"}, {endingKana: "う", result: "わ"},
+            {endingKana: "つ", result: "た"}],
 
-                negative: [
-                    {endingKana: "る", ichidanResult: "ない", godanResult: "らなかった"},
-                    {endingKana: "む", result: "まなかった"}, {endingKana: "く", result: "かなかった"},
-                    {endingKana: "ぐ", result: "がなかった"}, {endingKana: "ぶ", result: "ばなかった"},
-                    {endingKana: "ぬ", result: "ななかった"}, {endingKana: "う", result: "わなかった"},
-                    {endingKana: "つ", result: "たなかった"}]
-            }
-        },
+        eRow: [
 
-        past: {
-            polite: {
-                affirmative: [
-                    {endingKana: "る", ichidanResult: "ました", godanResult: "りました"},
-                    {endingKana: "む", result: "みました"}, {endingKana: "く", result: "きました"},
-                    {endingKana: "ぐ", result: "ぎました"}, {endingKana: "ぶ", result: "びました"},
-                    {endingKana: "ぬ", result: "にました"}, {endingKana: "う", result: "いました"},
-                    {endingKana: "つ", result: "ちました"},],
+            {endingKana: "る", result: "れ"},
+            {endingKana: "む", result: "め"}, {endingKana: "く", result: "け"},
+            {endingKana: "ぐ", result: "げ"}, {endingKana: "ぶ", result: "べ"},
+            {endingKana: "ぬ", result: "ね"}, {endingKana: "う", result: "え"},
+            {endingKana: "つ", result: "て"}],
 
-                negative: [
-                    {endingKana: "る", ichidanResult: "ませんでした", godanResult: "りませんでした"},
-                    {endingKana: "む", result: "みませんでした"}, {endingKana: "く", result: "きませんでした"},
-                    {endingKana: "ぐ", result: "ぎませんでした"}, {endingKana: "ぶ", result: "びませんでした"},
-                    {endingKana: "ぬ", result: "にませんでした"}, {endingKana: "う", result: "いませんでした"},
-                    {endingKana: "つ", result: "ちませんでした"}],
-            },
 
-            short: {
-                affirmative: [
-                    {endingKana: "る", ichidanResult: "る", godanResult: "る"},
-                    {endingKana: "む", result: "む"}, {endingKana: "く", result: "く"},
-                    {endingKana: "ぐ", result: "ぐ"}, {endingKana: "ぶ", result: "ぶ"},
-                    {endingKana: "ぬ", result: "ぬ"}, {endingKana: "う", result: "う"},
-                    {endingKana: "つ", result: "つ"}],
-
-                negative: [
-                    {endingKana: "る", ichidanResult: "ない", godanResult: "らない"},
-                    {endingKana: "む", result: "まない"}, {endingKana: "く", result: "かない"},
-                    {endingKana: "ぐ", result: "がない"}, {endingKana: "ぶ", result: "ばない"},
-                    {endingKana: "ぬ", result: "なない"}, {endingKana: "う", result: "わない"},
-                    {endingKana: "つ", result: "たない"}]
-            },
-        },
+        eRowTeForm: [
+            {endingKana: "る", ichidanResult: "られて", result: "れて"},
+            {endingKana: "む", result: "めて"}, {endingKana: "く", result: "けて"},
+            {endingKana: "ぐ", result: "げて"}, {endingKana: "ぶ", result: "べて"},
+            {endingKana: "ぬ", result: "ねて"}, {endingKana: "う", result: "えて"},
+            {endingKana: "つ", result: "てて"}],
 
         teForm: [
-            {endingKana: "る", ichidanResult: "て", godanResult: "って"},
+            {endingKana: "る", result: "って"},
             {endingKana: "む", result: "んで"}, {endingKana: "く", result: "いて"},
             {endingKana: "ぐ", result: "いで"}, {endingKana: "ぶ", result: "んで"},
             {endingKana: "ぬ", result: "んで"}, {endingKana: "う", result: "って"},
-            {endingKana: "つ", result: "って"}],
+            {endingKana: "つ", result: "って"}
+        ],
+}
+
+
+export const masuChanges = {
+    past: {
+        affirmative: "ました",
+        negative:  "ませんでした",
+    },
+
+    present: {
+        affirmative: "ます",
+        negative:  "ませせん",
     }
 }
+
+
 
     export const adjConjugations = {
 
